ee4a478035fb91bcc8b1b2fad0d80c678158e8a2
mpuk <puk.matej@gmail.com>
2017-10-01 15:44:07 +0200

    added subcommand for parsing, changed creating of lexer

--- soda/app.py
+++ soda/app.py
@@ -1,20 +1,30 @@
 import click
-from compiler import lexer
+from compiler import lexer, parser
+
+pass_lexer = click.make_pass_decorator(lexer.Lexer, ensure=True)
 
 
 @click.group()
 def main():
     pass
 
-
 @main.command(short_help='run lexical analysis')
 @click.argument('FILEPATH', type=click.Path(exists=True))
-def lex(filepath):
+@pass_lexer
+def lex(lexer, filepath):
     '''
     Run lexical analysis.
     '''
 
     file = open(filepath, 'r')
-    lex = lexer.Lexer()
 
-    lex.lexical_analysis(file)
+    lexer.lexical_analysis(file)
+
+@main.command(short_help='run parsing')
+@click.argument('FILEPATH', type=click.Path(exists=True))
+def parse(filepath):
+    '''
+    Run parsing.
+    '''
+
+    pass
