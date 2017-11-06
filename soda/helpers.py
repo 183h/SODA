a05e60cc9a32c674380570ffc9118e377d341a48
mpuk <puk.matej@gmail.com>
2017-11-06 17:01:08 +0100

    changed prepare_file to open file and pass it to lexer, parser

--- soda/helpers.py
+++ soda/helpers.py
@@ -1,24 +1,14 @@
 from os import remove
 
 
-def prepare_file(func):
+def open_file(func):
     def wrapper(*args, **kwargs):
-
-        print ("Preparing temp file...\n")
-
+        print ("Opening file...\n")
         file = open(kwargs["filepath"], 'r')
-        # data = file.read()
-        # file.close()
-        # stripped_data = data.replace("\n", "").replace(" ", "")
-        # temp_file = open(kwargs["filepath"] + ".temp", "w+")
-        # temp_file.write(stripped_data)
-        # temp_file.seek(0, 0)
 
         func(*args, file)
 
-        # temp_file.close()
-        # remove(kwargs["filepath"] + ".temp")
-
-        print ("\nDeleting temp file...")
+        print ("\Closing temp file...")
+        file.close()
 
     return wrapper
